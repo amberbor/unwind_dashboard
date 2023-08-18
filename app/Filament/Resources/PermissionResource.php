722364eb113440c64ffb4903525494edf0264c69
Amber <amber.borici@y42.com>
2023-08-18 11:10:56 +0200

    Fix the navigation for roles and permissions

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -23,6 +23,8 @@ class PermissionResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-key';
     protected static ?int $navigationSort = 3;
 
+    protected static ?string $navigationGroup = "Settings";
+
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -24,6 +24,8 @@ class RoleResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-finger-print';
     protected static ?int $navigationSort = 2;
 
+    protected static ?string $navigationGroup = "Settings";
+
 
     public static function form(Form $form): Form
     {
